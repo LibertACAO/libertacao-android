26e97234a055d75f587e6d68ef7c321729f3c9b1
guilhermekrz <guilhermekrz@gmail.com>
2015-09-04 17:19:55 -0300

    Added Parse Added push notification capabilities

---
+++ app/src/main/java/com/libertacao/libertacao/MyApp.java
@@ -0,0 +1,54 @@
+package com.libertacao.libertacao;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.Build;
+import android.os.StrictMode;
+
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+import com.parse.ParseUser;
+import com.squareup.leakcanary.LeakCanary;
+
+public class MyApp extends Application {
+    public static boolean IS_DEBUG = false;
+
+    // App context
+    private static Context context;
+
+    public static Context getAppContext() {
+        return MyApp.context;
+    }
+
+    public void onCreate() {
+        super.onCreate();
+        // TODO: add Fabric.with(this, new Crashlytics());
+        LeakCanary.install(this);
+        MyApp.context = getApplicationContext();
+        if (context.getPackageName().endsWith(".debug")) {
+            IS_DEBUG = true;
+        }
+
+        if(IS_DEBUG && Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
+        }
+
+        // TODO - ImageLoader.getInstance().init(ImageLoaderConfiguration.createDefault(this));
+
+        setupParse();
+    }
+
+    private void setupParse() {
+        // TODO: find a better way to hide credentials
+        Parse.initialize(this, "PUT_YOUR_CREDENTIALS_HERE", "PUT_YOUR_CREDENTIALS_HERE");
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+        ParseUser.enableAutomaticUser();
+        ParseUser.getCurrentUser().increment("RunCount");
+        ParseUser.getCurrentUser().saveInBackground();
+    }
+}
