cfe65de9e799e7307d1beae113b111cb093e8255
guilhermekrz <guilhermekrz@gmail.com>
2015-09-14 18:34:08 -0300

    Added simple profile name

--- app/src/main/java/com/libertacao/libertacao/view/perfil/PerfilActivity.java
+++ app/src/main/java/com/libertacao/libertacao/view/perfil/PerfilActivity.java
@@ -2,14 +2,20 @@ package com.libertacao.libertacao.view.perfil;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.libertacao.libertacao.R;
 import com.libertacao.libertacao.util.ViewUtils;
 import com.parse.ParseUser;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class PerfilActivity extends AppCompatActivity {
+    /**
+     * Interface elements
+     */
+    @InjectView(R.id.profile_username) TextView mProfileUsernameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +24,6 @@ public class PerfilActivity extends AppCompatActivity {
         ViewUtils.setHomeAsUpEnabled(this);
         ButterKnife.inject(this);
         ParseUser currentUser = ParseUser.getCurrentUser();
+        mProfileUsernameTextView.setText(currentUser.getUsername());
     }
 }
