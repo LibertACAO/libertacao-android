974f7436d4dc996acbf6509352a63521c4592912
guilhermekrz <guilhermekrz@gmail.com>
2015-09-14 21:52:31 -0300

    Added share button

--- app/src/main/java/com/libertacao/libertacao/view/notificacoes/NotificacaoParseQueryAdapter.java
+++ app/src/main/java/com/libertacao/libertacao/view/notificacoes/NotificacaoParseQueryAdapter.java
@@ -1,8 +1,10 @@
 package com.libertacao.libertacao.view.notificacoes;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.libertacao.libertacao.R;
@@ -19,7 +21,7 @@ public class NotificacaoParseQueryAdapter extends ParseQueryAdapter<Call> {
     }
 
     @Override
-    public View getItemView(Call object, View v, ViewGroup parent) {
+    public View getItemView(final Call object, View v, ViewGroup parent) {
         if (v == null) {
             v = View.inflate(getContext(), R.layout.row_notificacao, null);
         }
@@ -32,10 +34,24 @@ public class NotificacaoParseQueryAdapter extends ParseQueryAdapter<Call> {
             notificacao_image.loadInBackground();
         }
 
+        // Just hold title String attribute to onClick listener
+        final String title = object.getTitle();
+
         TextView notificacaoTitleView = (TextView) v.findViewById(R.id.notificacao_title);
-        notificacaoTitleView.setText(object.getTitle());
+        notificacaoTitleView.setText(title);
         TextView notificacaoSubtitleTextView = (TextView) v.findViewById(R.id.notificacao_subtitle);
         notificacaoSubtitleTextView.setText(object.getShortText());
+        Button btn_share_notification = (Button) v.findViewById(R.id.btn_share_notification);
+        btn_share_notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, title); // TODO: define a better text
+                sendIntent.setType("text/plain");
+                getContext().startActivity(sendIntent);
+            }
+        });
         return v;
     }
 }
