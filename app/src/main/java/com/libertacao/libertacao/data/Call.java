34d98c2ae20025c1e4d1a56aa45f524a49759b0c
guilhermekrz <guilhermekrz@gmail.com>
2015-09-16 17:42:09 -0300

    Added DataBinding library to test and to simplify implementation

--- app/src/main/java/com/libertacao/libertacao/data/Call.java
+++ app/src/main/java/com/libertacao/libertacao/data/Call.java
@@ -1,7 +1,11 @@
 package com.libertacao.libertacao.data;
 
+import android.databinding.BindingAdapter;
+
 import com.parse.ParseClassName;
+import com.parse.ParseFile;
 import com.parse.ParseGeoPoint;
+import com.parse.ParseImageView;
 import com.parse.ParseObject;
 
 @ParseClassName("Call")
@@ -10,6 +14,7 @@ public class Call extends ParseObject {
 
     }
 
+    @SuppressWarnings("unused")
     public String getDescription() {
         return getString("description");
     }
@@ -22,9 +27,25 @@ public class Call extends ParseObject {
         return getString("title");
     }
 
+    @SuppressWarnings("unused")
+    public ParseFile getImageFile(){
+        return getParseFile("image");
+    }
+
+    @SuppressWarnings("unused")
     public ParseGeoPoint getLocation() {
         return getParseGeoPoint("location");
         // TODO: add in fragment a map - http://stackoverflow.com/questions/26562848/how-to-integrate-google-maps-in-android-app
         // http://stackoverflow.com/questions/26174527/android-mapview-in-fragment
+    }
+
+    // Used by DataBinding library. TODO: This should ideally be in a separated ViewModel file
+    @SuppressWarnings("unused")
+    @BindingAdapter({"bind:imageFile"})
+    public static void loadImage(ParseImageView imageView, ParseFile imageFile) {
+        if (imageFile != null) {
+            imageView.setParseFile(imageFile);
+            imageView.loadInBackground();
+        }
     }
 }
--- app/src/main/java/com/libertacao/libertacao/view/notificacoes/NotificacaoDetail.java
+++ app/src/main/java/com/libertacao/libertacao/view/notificacoes/NotificacaoDetail.java
@@ -1,38 +1,24 @@
 package com.libertacao.libertacao.view.notificacoes;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.widget.TextView;
 
 import com.libertacao.libertacao.R;
 import com.libertacao.libertacao.data.Call;
+import com.libertacao.libertacao.databinding.ActivityNotificacaoDetailBinding;
 import com.libertacao.libertacao.util.ViewUtils;
 import com.parse.GetCallback;
 import com.parse.ParseException;
-import com.parse.ParseFile;
-import com.parse.ParseImageView;
 import com.parse.ParseQuery;
-
-import butterknife.ButterKnife;
-import butterknife.InjectView;
 
 public class NotificacaoDetail extends AppCompatActivity {
     public static final String NOTIFICACAO_OBJECT_ID = "NOTIFICACAO_OBJECT_ID";
 
-    /**
-     * Interface elements
-     */
-    @InjectView(R.id.notificacao_detail_image) ParseImageView mImageView;
-    @InjectView(R.id.notificacao_detail_title) TextView mTitleTextView;
-    @InjectView(R.id.notificacao_detail_subtitle) TextView mSubtitleTextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notificacao_detail);
-        ViewUtils.setHomeAsUpEnabled(this);
-        ButterKnife.inject(this);
         String notificacaoObjectId = getIntent().getStringExtra(NOTIFICACAO_OBJECT_ID);
         if(TextUtils.isEmpty(notificacaoObjectId)){
             //TODO: show an error message
@@ -43,13 +29,9 @@ public class NotificacaoDetail extends AppCompatActivity {
             query.getFirstInBackground(new GetCallback<Call>() {
                 @Override
                 public void done(Call object, ParseException e) {
-                    ParseFile imageFile = object.getParseFile("image");
-                    if (imageFile != null) {
-                        mImageView.setParseFile(imageFile);
-                        mImageView.loadInBackground();
-                    }
-                    mTitleTextView.setText(object.getTitle());
-                    mSubtitleTextView.setText(object.getShortText());
+                    ActivityNotificacaoDetailBinding binding = DataBindingUtil.setContentView(NotificacaoDetail.this, R.layout.activity_notificacao_detail);
+                    binding.setNotification(object);
+                    ViewUtils.setHomeAsUpEnabled(NotificacaoDetail.this);
                 }
             });
 
