462cbae5c02f2100744f7e80e0dc582d22b374cf
guilhermekrz <guilhermekrz@gmail.com>
2015-09-29 22:30:52 -0300

    Small changes

--- app/src/main/java/com/libertacao/libertacao/view/notificacoes/NotificacaoFragment.java
+++ app/src/main/java/com/libertacao/libertacao/view/notificacoes/NotificacaoFragment.java
@@ -2,13 +2,11 @@ package com.libertacao.libertacao.view.notificacoes;
 
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
