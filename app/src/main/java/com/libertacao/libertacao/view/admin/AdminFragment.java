ee1249c4ca990fadf5881ce984bfcac5f51da3b3
guilhermekrz <guilhermekrz@gmail.com>
2015-10-29 22:30:43 -0200

    Added menu to add event

--- app/src/main/java/com/libertacao/libertacao/view/admin/AdminFragment.java
+++ app/src/main/java/com/libertacao/libertacao/view/admin/AdminFragment.java
@@ -4,18 +4,42 @@ package com.libertacao.libertacao.view.admin;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.libertacao.libertacao.R;
 
 public class AdminFragment extends Fragment {
+    public static AdminFragment newInstance() {
+        return new AdminFragment();
+    }
+
     public AdminFragment() {
         // Required empty public constructor
     }
 
-    public static AdminFragment newInstance() {
-        return new AdminFragment();
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.admin_fragment_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_add_event:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
