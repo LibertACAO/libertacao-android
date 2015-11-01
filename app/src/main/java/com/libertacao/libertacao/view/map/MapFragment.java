e04054d02b33d67b9d5a48237fea38b7177b6bf8
guilhermekrz <guilhermekrz@gmail.com>
2015-11-01 18:17:51 -0200

    Added edit mode to MapFragment

--- app/src/main/java/com/libertacao/libertacao/view/map/MapFragment.java
+++ app/src/main/java/com/libertacao/libertacao/view/map/MapFragment.java
@@ -1,6 +1,7 @@
 package com.libertacao.libertacao.view.map;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Toast;
 
@@ -23,16 +24,25 @@ import com.libertacao.libertacao.persistence.DatabaseHelper;
 public class MapFragment extends SupportMapFragment implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         GoogleMap.OnInfoWindowClickListener,
+        GoogleMap.OnMapClickListener,
         GoogleMap.OnMarkerClickListener {
 
     private static final String EVENT_ID = "EVENT_ID";
+    private static final String EDIT_MODE = "EDIT_MODE";
     private GoogleApiClient mGoogleApiClient;
-    private Event event;
+    @Nullable private Event event;
+    private boolean editMode;
+    @Nullable private Marker eventLocationMarker;
 
     public static MapFragment newInstance(Event event) {
+        return newInstance(event, false);
+    }
+
+    public static MapFragment newInstance(Event event, boolean editMode) {
         MapFragment mapFragment = new MapFragment();
         Bundle bundle = new Bundle();
         bundle.putInt(EVENT_ID, event.getId());
+        bundle.putBoolean(EDIT_MODE, editMode);
         mapFragment.setArguments(bundle);
         return mapFragment;
     }
@@ -41,12 +51,13 @@ public class MapFragment extends SupportMapFragment implements GoogleApiClient.C
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        editMode = getArguments().getBoolean(EDIT_MODE, false);
         int eventId = getArguments().getInt(EVENT_ID, -1);
         if(eventId != -1) {
             event = DatabaseHelper.getHelper(getContext()).getEventIntegerRuntimeExceptionDao().queryForId(eventId);
         }
 
-        if(event == null) {
+        if(event == null && !editMode) {
             Toast.makeText(getContext(), getString(R.string.eventNotFound), Toast.LENGTH_LONG).show();
         } else {
             mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
@@ -63,6 +74,9 @@ public class MapFragment extends SupportMapFragment implements GoogleApiClient.C
         if(getMap() != null) {
             getMap().setOnMarkerClickListener(this);
             getMap().setOnInfoWindowClickListener(this);
+            if(editMode) {
+                getMap().setOnMapClickListener(this);
+            }
         }
     }
 
@@ -86,25 +100,31 @@ public class MapFragment extends SupportMapFragment implements GoogleApiClient.C
     }
 
     private void initCamera() {
-        LatLng eventLocation = new LatLng(event.getLatitude(), event.getLongitude());
-        CameraPosition position = CameraPosition.builder()
-                .target(eventLocation)
-                .zoom(16f)
-                .bearing(0.0f)
-                .tilt(0.0f)
-                .build();
-
-        getMap().animateCamera(CameraUpdateFactory.newCameraPosition(position), null);
         getMap().setMapType(GoogleMap.MAP_TYPE_NORMAL);
         getMap().setTrafficEnabled(true);
         getMap().setMyLocationEnabled(true);
         getMap().getUiSettings().setZoomControlsEnabled(true);
 
-        // Create marker at event location
-        MarkerOptions options = new MarkerOptions().position(eventLocation);
-        options.title(event.getTitle());
-        options.icon(BitmapDescriptorFactory.defaultMarker());
-        getMap().addMarker(options);
+        if(event != null && event.hasLocation()) {
+            LatLng eventLocation = new LatLng(event.getLatitude(), event.getLongitude());
+            CameraPosition position = CameraPosition.builder()
+                    .target(eventLocation)
+                    .zoom(16f)
+                    .bearing(0.0f)
+                    .tilt(0.0f)
+                    .build();
+
+            getMap().animateCamera(CameraUpdateFactory.newCameraPosition(position), null);
+
+            // Create marker at event location
+            MarkerOptions options = new MarkerOptions().position(eventLocation);
+            options.title(event.getTitle());
+            if (editMode) {
+                options.draggable(true);
+            }
+            options.icon(BitmapDescriptorFactory.defaultMarker());
+            eventLocationMarker = getMap().addMarker(options);
+        }
     }
 
     @Override
@@ -126,5 +146,24 @@ public class MapFragment extends SupportMapFragment implements GoogleApiClient.C
     public boolean onMarkerClick(Marker marker) {
         marker.showInfoWindow();
         return true;
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        if(eventLocationMarker != null) {
+            eventLocationMarker.remove();
+        }
+
+        MarkerOptions options = new MarkerOptions().position(latLng);
+        if(event != null) {
+            options.title(event.getTitle());
+        }
+        options.draggable(true);
+        options.icon( BitmapDescriptorFactory.defaultMarker() );
+        eventLocationMarker = getMap().addMarker(options);
+    }
+
+    @Nullable public LatLng getSelectedLatLng() {
+        return eventLocationMarker != null? eventLocationMarker.getPosition() : null;
     }
 }
