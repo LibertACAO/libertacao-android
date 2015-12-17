1b11b89b3ddae26de3f5a43b57b8e37b7e172633
guilhermekrz <guilhermekrz@gmail.com>
2015-12-17 21:19:12 -0200

    Adjusting PushNoticationManager

--- app/src/main/java/com/libertacao/libertacao/push/PushNotificationManager.java
+++ app/src/main/java/com/libertacao/libertacao/push/PushNotificationManager.java
@@ -16,35 +16,24 @@ class PushNotificationManager {
     }
 
     private final AtomicInteger notificationCount = new AtomicInteger(0);
-    private volatile boolean shouldShowNotifications = true;
-
-    public void setShouldShowNotifications(boolean show) {
-        shouldShowNotifications = show;
-    }
-
-    public int getNotificationCount() {
-        return notificationCount.get();
-    }
 
     public void showNotification(Context context, Notification notification) {
-        if (context != null && notification != null) {
+        if (context != null && notification != null && notification.contentView != null) {
             notificationCount.incrementAndGet();
 
-            if (shouldShowNotifications) {
-                // Fire off the notification
-                NotificationManager nm =
-                        (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            // Fire off the notification
+            NotificationManager nm =
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-                // Pick an id that probably won't overlap anything
-                int notificationId = (int)System.currentTimeMillis();
+            // Pick an id that probably won't overlap anything
+            int notificationId = (int)System.currentTimeMillis();
 
-                try {
-                    nm.notify(notificationId, notification);
-                } catch (SecurityException e) {
-                    // Some phones throw an exception for unapproved vibration
-                    notification.defaults = Notification.DEFAULT_LIGHTS | Notification.DEFAULT_SOUND;
-                    nm.notify(notificationId, notification);
-                }
+            try {
+                nm.notify(notificationId, notification);
+            } catch (SecurityException e) {
+                // Some phones throw an exception for unapproved vibration
+                notification.defaults = Notification.DEFAULT_LIGHTS | Notification.DEFAULT_SOUND;
+                nm.notify(notificationId, notification);
             }
         }
     }
